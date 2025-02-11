37fcd39a8c845ef4dfb635f430131eceb6b2a569
Biniam Garomsa <biniamgaromsa@gmail.com>
2025-02-11 11:59:33 -0500

    update to infer on set median threshold

--- src/BrainIAC/OverallSurvival/infer_os.py
+++ src/BrainIAC/OverallSurvival/infer_os.py
@@ -76,17 +76,6 @@ class OSInference(BaseConfig):
     ## spin up data loaders
     def setup_data(self):
         config = self.get_config()
-        self.train_dataset = OSDataset(
-            csv_path=config['data']['train_csv'],
-            root_dir=config["data"]["root_dir"]
-        )
-        self.train_loader = DataLoader(
-            self.train_dataset,
-            batch_size=1,
-            shuffle=False,
-            num_workers=1
-        )
-        
         self.test_dataset = OSDataset(
             csv_path=config["data"]["test_csv"],
             root_dir=config["data"]["root_dir"]
@@ -101,76 +90,64 @@ class OSInference(BaseConfig):
             
     def infer(self):
         """
-        Run inference pass for training set and test set
-        Inference on training set to determine classification
-        threshold for risk stratification
+        Run inference pass for test set
         Returns:
             dict: Dictionary with evaluation metrics
         """
         
-        def perform_inference(data_loader, dataset_name):
-            results_df = pd.DataFrame(columns=['PredictedProb', 'PredictedLabel', 'PredictedRisk', 'TrueLabel', 'pat_id'])
-            all_labels = []
-            all_predictions = []
-            all_probs = []
-            
-            with torch.no_grad():
-                for sample in tqdm(data_loader, desc=f"Inference ({dataset_name})", unit="batch"):
-                    inputs = sample['image'].to(self.device)
-                    labels = sample['label'].float().to(self.device)
-                    pat_ids = sample['pat_id']
-                    with autocast():
-                        outputs = self.model(inputs)
-                    
-                    probs = torch.sigmoid(outputs).cpu().numpy().flatten()
-                    preds = (probs > 0.5).astype(int)
-                    
-                    all_labels.extend(labels.cpu().numpy().flatten())
-                    all_predictions.extend(preds)
-                    all_probs.extend(probs)
-                    
-                    result = pd.DataFrame({
-                        'PredictedProb': probs,
-                        'PredictedLabel': preds,
-                        'PredictedRisk': 1.0 - probs,
-                        'TrueLabel': labels.cpu().numpy().flatten(),
-                        'pat_id': pat_ids
-                    })
-                    results_df = pd.concat([results_df, result], ignore_index=True)
-
-            # log metrics 
-            metrics = calculate_metrics(
-                np.array(all_probs),
-                np.array(all_predictions),
-                np.array(all_labels)
-            )
-            
+        results_df = pd.DataFrame(columns=['PredictedProb', 'PredictedLabel', 'PredictedRisk', 'TrueLabel', 'pat_id'])
+        all_labels = []
+        all_predictions = []
+        all_probs = []
         
-            print("\nTest Set Metrics:")
-            print(f"Accuracy: {metrics['accuracy']:.4f}")
-            print(f"Precision: {metrics['precision']:.4f}")
-            print(f"Recall: {metrics['recall']:.4f}")
-            print(f"F1 Score: {metrics['f1']:.4f}")
-            print(f"AUC: {metrics['auc']:.4f}")
-            
+        with torch.no_grad():
+            for sample in tqdm(self.test_loader, desc=f"Inference on test data", unit="batch"):
+                inputs = sample['image'].to(self.device)
+                labels = sample['label'].float().to(self.device)
+                pat_ids = sample['pat_id']
+                with autocast():
+                    outputs = self.model(inputs)
+                
+                probs = torch.sigmoid(outputs).cpu().numpy().flatten()
+                preds = (probs > 0.5).astype(int)
+                
+                all_labels.extend(labels.cpu().numpy().flatten())
+                all_predictions.extend(preds)
+                all_probs.extend(probs)
+                
+                result = pd.DataFrame({
+                    'PredictedProb': probs,
+                    'PredictedLabel': preds,
+                    'PredictedRisk': 1.0 - probs,
+                    'TrueLabel': labels.cpu().numpy().flatten(),
+                    'pat_id': pat_ids
+                })
+                results_df = pd.concat([results_df, result], ignore_index=True)
 
-            
-            return results_df
+        # log metrics 
+        metrics = calculate_metrics(
+            np.array(all_probs),
+            np.array(all_predictions),
+            np.array(all_labels)
+        )
+        
+        print("\nTest Set Metrics:")
+        print(f"Accuracy: {metrics['accuracy']:.4f}")
+        print(f"Precision: {metrics['precision']:.4f}")
+        print(f"Recall: {metrics['recall']:.4f}")
+        print(f"F1 Score: {metrics['f1']:.4f}")
+        print(f"AUC: {metrics['auc']:.4f}")
 
-        infer_on_train = perform_inference(self.train_loader, "Train")        
-        infer_on_test = perform_inference(self.test_loader, "Test")
-
+        # Save results
+        results_df.to_csv('./data/output/overall_survival_predictions.csv', index=False)
+   
+        # KM analysis on test set
         clinical_df = pd.read_csv("../clinical/clinical_filtered.csv")
-        clinical_df = clinical_df[["pat_id", "survival_years", "deadstatus_event", "survival"]]
-        split_median = infer_on_train["PredictedRisk"].median()
-        merged_results = infer_on_test.merge(clinical_df,how = "left")[["pat_id", "PredictedRisk", "survival_years","deadstatus_event", "survival"]]
+        clinical_df = clinical_df[["pat_id", "survival_years", "deadstatus_event", "survival"]]         
+        split_median = 0.5215  # median threshold calculated using validation data      
+        merged_results = results_df.merge(clinical_df,how = "left")[["pat_id", "PredictedRisk", "survival_years","deadstatus_event", "survival"]]
         merged_results['group'] = merged_results["PredictedRisk"].apply(lambda x: 'Low Risk' if x < split_median  else 'High Risk')
         _ = plot_km_curve(merged_results)
-        
-        # Save results
-        infer_on_test.to_csv('./data/output/mci_classification_predictions.csv', index=False)
-
-
 
 
 if __name__ == "__main__":
